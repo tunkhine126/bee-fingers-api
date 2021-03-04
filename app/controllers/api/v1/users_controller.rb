06bcbb409eb29ba32380e31ebdff201760b7a8d7
Tun Khine <tunkhine126@gmail.com>
2021-03-03 21:38:23 -0500

    Add User CRUD and password column

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,41 @@
 class Api::V1::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+
+    redirect_to user_path(user)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @user.update(user_params)
+
+    redirect_to users_path(@user)
+  end
+
+  def destroy
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_033843) do
+ActiveRecord::Schema.define(version: 2021_03_04_022447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_033843) do
     t.string "uuid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password"
   end
 
 end
